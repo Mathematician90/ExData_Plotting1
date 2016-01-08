a87a59ec1c37b0c6f448688647ee3ab34e9c30f3
Mathematician90 <lioniza1@yandex.ru>
2016-01-08 22:00:53 +0300

    course project week 1

--- plot2.R
+++ plot2.R
@@ -4,6 +4,6 @@ household_power_consumption <- read.csv('household_power_consumption.txt', heade
 household_power_consumption$Date<-as.Date(household_power_consumption$Date,"%d/%m/%Y")
 household_power_consumption<-household_power_consumption[which(household_power_consumption$Date=="2007-02-01"|household_power_consumption$Date=="2007-02-02"),]
 household_power_consumption$Dateandtime <- as.POSIXct(paste(as.Date(household_power_consumption$Date), household_power_consumption$Time))
-plot(household_power_consumption$Global_active_power~household_power_consumption$Dateandtime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+plot(household_power_consumption$Global_active_power,household_power_consumption$Dateandtime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
--- plot3.R
+++ plot3.R
@@ -9,6 +9,5 @@ plot(household_power_consumption$Dateandtime, household_power_consumption$Sub_me
 lines(household_power_consumption$Dateandtime, household_power_consumption$Sub_metering_1)
 lines(household_power_consumption$Dateandtime, household_power_consumption$Sub_metering_2, col="red")
 lines(household_power_consumption$Dateandtime, household_power_consumption$Sub_metering_3, col="blue")
-legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       col= c("black", "red","blue"),lty=1, lwd=1)
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col= c("black", "red","blue"),lty=1, lwd=1)
 dev.off()
